b79b71b8e3ad1fc286c6e854f173020693fc48a6
ytyaru <yttry0@gmail.com>
2020-10-26 15:27:36 +0900

    Created a repository

---
+++ note/note.md
@@ -0,0 +1,1 @@
+* https://fits.hatenablog.com/entry/20130224/1361715505
---
+++ src/1/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Ceu
+#---------------------------------------------------------------------------
+# CreatedAt: 2020-10-26 14:51:32
+#---------------------------------------------------------------------------
+Run() {
+	THIS="$(realpath "${BASH_SOURCE:-0}")"; HERE="$(dirname "$THIS")"; PARENT="$(dirname "$HERE")"; THIS_NAME="$(basename "$THIS")"; APP_ROOT="$PARENT";
+	cd "$HERE"
+#	csc Program.cs Sub.cs
+	csc *.cs
+	mono Program.exe
+}
+Run "$@"
